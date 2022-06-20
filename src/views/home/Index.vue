0699cc699bd5bc8724e50f93cdd755e3b1d9d576
xytao <2431352054@qq.com>
2022-06-20 13:29:12 +0800

    清除垃圾

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,19 +1,54 @@
 <template>
   <div class="toy-home">
-    <el-button @click="changeTitle">change</el-button>
+    <div class="find-list">
+      <el-input v-model="inputValue" @keydown.enter="findDetails"></el-input>
+      <el-button type="primary" @click="findDetails">查找</el-button>
+    </div>
+    <div class="content-A">
+      <img src="" alt="">
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { onMounted } from 'vue';
+  import { onMounted, ref } from 'vue';
 
   onMounted(() => {
 
   })
+
+  const inputValue = ref<any>()
+  const showA = ref<boolean>(false)
+
+  const findDetails = () =>{
+    if (inputValue.value === 404) {
+      showA.value = true
+    } else showA.value = false
+  }
 </script>
 
 <style scoped lang="less">
   .toy-home {
-    width: 100vw;
+
+    .find-list {
+      display: flex;
+      justify-content: center;
+      margin-top: 50px;
+
+      :deep(.el-input) {
+        width: 220px;
+        height: 32px;
+
+        .el-input_inner {
+          height: 32px;
+          line-height: 32px;
+          padding: 0 10px;
+        }
+      }
+      :deep(.el-button) {
+        min-height: 32px;
+        padding: 8px 20px;
+      }
+    }
   }
 </style>
